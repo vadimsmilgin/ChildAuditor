8cea2133e562dee7ec170137f3908968b44d6904
Admin <admin@MacBook-Pro-Admin.local>
2019-10-29 21:19:08 +0300

    aura component was completed

--- force-app/main/default/aura/childAuditor/childAuditorController.js
+++ force-app/main/default/aura/childAuditor/childAuditorController.js
@@ -1,11 +1,19 @@
 ({
-    refresh : function(component, event, helper) {
-        var action = component.get("c.getAllRelatedChildren");
+    doInit : function(component, event, helper) {
+        var action = component.get("c.getAllRelatedChild");
+        var recordId = component.get("v.recordId");
+
+        action.setParams({"recordId": recordId});
         
         action.setCallback(this, function(response) {
             var state = response.getState();
             if (state === "SUCCESS") {
-                component.set("v.expenses", response.getReturnValue());
+                var custs = [];
+                var conts = response.getReturnValue();
+                for(var key in conts){
+                    custs.push({value:conts[key], key:key});
+                }
+                component.set("v.childs", custs);
             }
             else {
                 console.log("Failed with state: " + state);
