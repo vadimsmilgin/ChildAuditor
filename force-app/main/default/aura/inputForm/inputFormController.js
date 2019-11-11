281e7ec0ffec2bad61360a0bdcd80e8daa3adae4
Admin <admin@MacBook-Pro-Admin.local>
2019-11-11 17:22:04 +0300

    was added new field DateCreated (comments need to replace in own component)

--- force-app/main/default/aura/inputForm/inputFormController.js
+++ force-app/main/default/aura/inputForm/inputFormController.js
@@ -1,8 +1,10 @@
 ({
     postMessage : function(component, event, helper) {
         var message = component.get("v.message");
+        var emptyString = "";
         var showComment = component.getEvent("showComment");
         showComment.setParams({ "message": message });
+        component.set('v.message', emptyString);
         showComment.fire();
     }
 })
--- force-app/main/default/aura/outputPlaceForMessages/outputPlaceForMessagesController.js
+++ force-app/main/default/aura/outputPlaceForMessages/outputPlaceForMessagesController.js
@@ -1,13 +1,39 @@
-({
-    checkAvatar : function(component, event, helper) {
-        var action = component.get("c.fetchUserDetail");
-        var message = event.getParam("message");
+({  
+    doInit : function(component, event, helper) {
+        var action = component.get("c.getChat");
+        var recordId = component.get("v.recordId");
+
+        action.setParams({"recordId": recordId});
+
         action.setCallback(this, function(response) {
             var state = response.getState();
             if (state === "SUCCESS") {
-                var res = response.getReturnValue();
-                component.set('v.currentUser', res);
-                component.set('v.message', message);
+                var conts = response.getReturnValue();
+                console.log(conts);
+                var dateForMessages = [];
+                for(var i in conts) {
+                    dateForMessages.push({value: conts[i].DateCreated__c});                   
+                }
+                console.log(dateForMessages);
+                component.set("v.chat", conts);
+            }
+        });
+        $A.enqueueAction(action);
+    },
+
+    addComment : function(component, event, helper) {       
+        var action = component.get("c.addCommentInObject");
+        var message = event.getParam("message");
+        var recordId = component.get("v.recordId");
+        var userId = $A.get("$SObjectType.CurrentUser.Id");
+
+        action.setParams({"recordId": recordId, "message": message, "idUser": userId});
+
+        action.setCallback(this, function(response) {
+            var state = response.getState();
+            if (state === "SUCCESS") {
+                var conts = response.getReturnValue();
+                component.set("v.chat", conts);
             }
         });
         $A.enqueueAction(action); 
